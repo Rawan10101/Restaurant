a195470d948b37621f7df95558fd5dc54b09e342
Rawan10101 <145160003+Rawan10101@users.noreply.github.com>
2024-10-20 22:31:53 +0300

    Main outlines

---
+++ main3.js
@@ -0,0 +1,13 @@
+// Example of simple JavaScript functionality
+// You can add more interactivity later
+
+// Smooth scrolling when clicking navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
